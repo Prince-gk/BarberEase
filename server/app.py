8d016d550820013fb8794d29d84c870db683c889
Prince-gk <prince.georanja@gmail.com>
2025-06-29 22:00:10 +0300

    fix: Add missing TestAPI resource route in app.py

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@ from flask import request
 from flask_restful import Resource
 from resources import (
     ClientList,
+    TestAPI,
     BarberList,
     ServiceList,
     ReviewList,
@@ -16,7 +17,7 @@ from models import Client, Barber, Service, Appointment, Review
 
 
 # Routes
-
+api.add_resource(TestAPI, "/")
 api.add_resource(ClientList, "/clients")
 api.add_resource(BarberList, "/barbers")
 api.add_resource(ServiceList, "/services")
