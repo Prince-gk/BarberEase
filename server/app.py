ce1c0a5529172efbc00a361b230b4f1339c04667
Prince-gk <prince.georanja@gmail.com>
2025-06-29 21:51:26 +0300

    fix: Correct SQLAlchemy version in requirements.txt and remove unnecessary blank line in app.py

--- server/app.py
+++ server/app.py
@@ -17,7 +17,6 @@ api.add_resource(AppointmentList, "/appointments")
 api.add_resource(AppointmentDetail, "/appointments/<int:id>")
 
 
-
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
 
